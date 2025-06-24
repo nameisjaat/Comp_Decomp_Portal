9b7c9d1521b69e9ba40631f4e322a4884b955fde
nameisjaat <garvitchoudhary3101@gmail.com>
2025-06-25 04:15:42 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,7 @@ import { rleRouter } from "./routes/rleRoutes.js";
 import { lz77Router } from "./routes/lz77Routes.js";
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 //middleware
 app.use(express.json());
